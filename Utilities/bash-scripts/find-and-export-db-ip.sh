aa5bcf29ece8188422219bb68ff3eccfbef39772
peferso <p.fdez.s.90@gmail.com>
2022-02-27 11:52:40 +0100

    Corrections to use AWS CLI to retrieve public IP

--- Utilities/bash-scripts/find-and-export-db-ip.sh
+++ Utilities/bash-scripts/find-and-export-db-ip.sh
@@ -2,42 +2,48 @@
 
 MAINDIR=$PEGASO_INFRA_DIR
 
+shopt -s expand_aliases
 
-  echo -ne 'Finding '${ec2_type}' public IP... |#   | - Refreshing terraform state                       \r'; sleep 1
+source ~/.profile_PEGASO
+
+echo -ne 'Finding '${ec2_type}' public IP... |#   | - Using AWS CLI \r'; sleep 1
 
 #find_and_export_db_ip () { 
 
   cd ${MAINDIR} 
-  
-  terraform refresh
 
-  outputList="$(terraform output)" 
-  outputList=${outputList//' = '/'='} 
-  outputList=$(echo "$outputList" | tr '\n' ' ') 
-  outputList=${outputList//[' "']/} 
-  outputList=(${outputList//']'/'] '}) 
-  delimiter="=" 
-  ec2_type="EC2Database"
-  modNames=(); 
-  insPubIP=(); 
+  publicIP=$(get_db_ip)
 
-  for element in "${outputList[@]}" 
-  do 
-
-    partLeftt=${element%%"${delimiter}"*} 
-    partRight=${element#*"${delimiter}"} 
-    partRight=$( echo  "${partRight//['\"\,\[\]']/}"  ) 
-
-    if [[ ${partLeftt} == *"PublicIP"* ]] && [[ ${partLeftt} == *"${ec2_type}"* ]];  
-    then 
-
-      publicIP=${partRight} 
-
-      echo -ne 'Finding '${ec2_type}' public IP... |##  | - IP '${publicIP}' was found                        \r'; sleep 1
-
-    fi 
-
-  done 
+ # 
+  #terraform refresh
+#
+  #outputList="$(terraform output)" 
+  #outputList=${outputList//' = '/'='} 
+  #outputList=$(echo "$outputList" | tr '\n' ' ') 
+  #outputList=${outputList//[' "']/} 
+  #outputList=(${outputList//']'/'] '}) 
+  #delimiter="=" 
+  #ec2_type="EC2Database"
+ # modNames=(); 
+ # insPubIP=(); 
+#
+ # for element in "${outputList[@]}" 
+ # do 
+#
+ #   partLeftt=${element%%"${delimiter}"*} 
+ #   partRight=${element#*"${delimiter}"} 
+ #   partRight=$( echo  "${partRight//['\"\,\[\]']/}"  ) 
+#
+ #   if [[ ${partLeftt} == *"PublicIP"* ]] && [[ ${partLeftt} == *"${ec2_type}"* ]];  
+ #   then 
+#
+#      publicIP=${partRight} 
+#
+#      echo -ne 'Finding '${ec2_type}' public IP... |##  | - IP '${publicIP}' was found                        \r'; sleep 1
+#
+#    fi 
+#
+#  done 
 
   cd - > /dev/null  
 
